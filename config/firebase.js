4b8b4907d0a534ddced3880d364bedc3c134c0a0
Esha Chawda <eshachawda18@gmail.com>
2025-06-20 13:30:00 -0400

    Fix Firebase key path for Render deployment

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseServiceAccountKey.json");
+//const serviceAccount = require("./firebaseServiceAccountKey.json");
+const path = require("path");
+const serviceAccount = require(path.join(
+  process.env.FIREBASE_CREDENTIAL_PATH || "./config/firebaseServiceAccountKey.json"
+));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
